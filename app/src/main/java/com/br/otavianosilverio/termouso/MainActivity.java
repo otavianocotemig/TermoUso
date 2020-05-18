02755284cc14b03d97590b49b20d4cbf8d844a7e
otavianocotemig <otaviano@cotemig.com.br>
2020-05-18 10:52:56 -0300

    Patterns.
    Patterns

--- app/src/main/java/com/br/otavianosilverio/termouso/MainActivity.java
+++ app/src/main/java/com/br/otavianosilverio/termouso/MainActivity.java
@@ -1,9 +1,11 @@
 package com.br.otavianosilverio.termouso;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity ;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.mViewMain.chec_aceite = this.findViewById(R.id.check_aceite);
         this.mViewMain.button_salvar = this.findViewById(R.id.button_salvar);
         this.mViewMain.rd_sexo = this.findViewById(R.id.Rd_sexo);
+        this.mViewMain.edit_email = this.findViewById(R.id.edit_email);
 
         // Identifica os objetos que são utilizados para ação
         this.mViewMain.button_salvar.setOnClickListener(this);
@@ -34,35 +37,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Botao salvar
         if (view.getId() == R.id.button_salvar){
             // Verificar se nome foi preenchido
-            if (TextUtils.isEmpty(this.mViewMain.edit_nome.getText().toString())){
-                this.mViewMain.edit_nome.setError("Informe o nome");
-                this.mViewMain.edit_nome.requestFocus();
-            }
-
-            if (!this.mViewMain.chec_aceite.isChecked()){
-                this.mViewMain.chec_aceite.setError("Você precisa concordar com os termos");
-                this.mViewMain.chec_aceite.requestFocus();
-
-            }
-            // verificar o botao do sexo preenchido
-            int itemRadioGroupSelecionado = this.mViewMain.rd_sexo.getCheckedRadioButtonId();
-
-            if (itemRadioGroupSelecionado == -1){
-                this.mViewMain.chec_aceite.setError("Você precisa definir o sexo");
-                this.mViewMain.chec_aceite.requestFocus();
-            }
-
+           if (validaCampos() == false){
+               MensagemDeErro("Os dados estão incorretos. Verifique");
+           }else{
+               MensagemDeErro("Dados corretos");
+           }
 
         }
         //Botao sair
 
     }
 
+    private boolean isEmailValido(String email){
 
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean validaCampos(){
+        boolean resultado = false;
+
+        if (resultado = TextUtils.isEmpty(this.mViewMain.edit_nome.getText().toString())){
+            this.mViewMain.edit_nome.requestFocus();
+        }
+        if (resultado = !isEmailValido(this.mViewMain.edit_email.getText().toString()))
+        {
+            this.mViewMain.edit_email.setError("Email inválido");
+            this.mViewMain.edit_email.requestFocus();
+        }
+
+
+        if (!this.mViewMain.chec_aceite.isChecked()) {
+            resultado = false;
+        }else{
+            resultado = true;
+        }
+        // verificar o botao do sexo preenchido
+        int itemRadioGroupSelecionado = this.mViewMain.rd_sexo.getCheckedRadioButtonId();
+
+        if (itemRadioGroupSelecionado == -1) {
+            resultado = false;
+        }else{
+            resultado = true;
+        }
+
+        return resultado;
+
+    }
+
+    private void MensagemDeErro(String mensagem){
+        AlertDialog.Builder Msg = new AlertDialog.Builder(this);
+        Msg.setMessage(mensagem);
+        Msg.setNeutralButton("OK",null);
+        Msg.create();
+        Msg.show();
+
+    }
     private static class ViewHolder{
         private EditText edit_nome;
         private Button button_salvar;
         private CheckBox chec_aceite;
         private RadioGroup  rd_sexo;
+        private EditText edit_email;
     }
 }
